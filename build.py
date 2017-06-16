5bf5eb7cd2266802698f2b55a4a7a9d2bc9f5a0f
harishkandan <harishkandan95@gmail.com>
2017-06-16 18:56:30 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,16 @@
 def solution(dic1, dic2):
-    """Enter Code Here"""
+    dic1_keys=dic1.keys()
+    dic2_keys=dic2.keys()
+    ans={}
+    for i in range(0,len(dic1)):
+        if(dic1_keys[i] in dic2_keys):
+            ans[dic1_keys[i]] = dic1.get(dic1_keys[i]) + dic2.get(dic1_keys[i])
+        else:
+            ans[dic1_keys[i]] = dic1.get(dic1_keys[i])
+    for i in range(0,len(dic2)):
+        if(dic2_keys[i] not in dic1_keys):
+            ans[dic2_keys[i]] = dic2.get(dic2_keys[i])
+    return ans
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+print solution(d1,d2)
