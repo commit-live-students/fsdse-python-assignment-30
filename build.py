6b470b62bbde702b646b7bc7735aecdc3eb86c63
datanomnom <ronzzz92@gmail.com>
2017-06-17 18:42:13 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,6 @@
+from collections import Counter
 def solution(dic1, dic2):
-    """Enter Code Here"""
+    dic1 = {'a': 100, 'b': 200, 'c':300}
+    dic2 = {'a': 300, 'b': 200, 'd':400}
+    dic3 = Counter(dic1) + Counter(dic2)
+    return dic3
