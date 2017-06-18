73df3b1840de657f73e6bc4648cb993e954e2920
birajparikh16 <birajparikh16@gmail.com>
2017-06-18 19:35:34 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,15 @@
+"""Example:
+
+Input
+
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+
+Output
+
+{'b': 400, 'd': 400, 'a': 400, 'c': 300}"""
+from collections import Counter
 def solution(dic1, dic2):
-    """Enter Code Here"""
+    d = 0
+    d = Counter(dic1) + Counter(dic2)
+    return d
