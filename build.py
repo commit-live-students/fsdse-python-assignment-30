d147d5c92d0b7c9361769708271b0fb5798b3da8
kaushikb11 <kaushikbokka@gmail.com>
2017-06-18 04:56:20 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,11 @@
 def solution(dic1, dic2):
-    """Enter Code Here"""
+    d={}
+    for k in dic1.keys():
+        if k in dic2.keys():
+            d[k]=dic1[k]+dic2[k]
+        else:
+            d[k]=dic1[k]
+    for k in dic2.keys():
+        if k not in dic1.keys():
+            d[k]=dic2[k]
+    return d
