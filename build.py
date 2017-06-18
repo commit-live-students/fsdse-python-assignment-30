849fa4b4fa10776f16e56416434ebb5915c31181
Bicky23 <bijitdeka23@gmail.com>
2017-06-18 16:16:25 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,13 @@
 def solution(dic1, dic2):
     """Enter Code Here"""
+    for x in dic1.keys():
+        if x in dic2.keys():
+            dic1[x] = dic1[x] + dic2[x]
+        else:
+            pass
+    for x in dic2.keys():
+        if x in dic1.keys():
+            pass
+        else:
+            dic1[x] = dic2[x]
+    return dic1
