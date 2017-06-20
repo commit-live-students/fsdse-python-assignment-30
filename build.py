6e39b711db3ca7af0fccdc0e62b960acfeccb60f
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-20 15:06:13 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
+from collections import Counter
+
 def solution(dic1, dic2):
-    """Enter Code Here"""
+    d3 = Counter(dic1) + Counter(dic2)
+    d = {}
+    for key, value in d3.items():
+        d[key] = value
+    return d
+
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+solution(d1,d2)
