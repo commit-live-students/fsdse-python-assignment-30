c0f6ac7226c66c2e2f58b170af038b55943cd0ea
sagarambalam <sagar.ambalam@gmail.com>
2017-06-21 03:46:30 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,13 @@
 def solution(dic1, dic2):
-    """Enter Code Here"""
+    dic3 = {}
+    l1 = dic1.keys()
+    l2 = dic2.keys()
+    for e in l1:
+        if e in l2:
+            dic3[e] = dic1[e] + dic2[e]
+        else:
+            dic3[e] = dic1[e]
+    for e in l2:
+        if e not in l1:
+            dic3[e] = dic2[e]
+    return dic3 
