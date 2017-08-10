c7eeb5486f67de806b4bb583c216ee019217fac9
SiddharthPant <spsiddharthpant@gmail.com>
2017-08-10 09:07:02 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,7 @@
 def solution(dic1, dic2):
-    """Enter Code Here"""
+    return {k: dic1.get(k, 0) + dic2.get(k, 0) for k in set(dic1) | set(dic2)}
+
+
+d1 = {'a': 100, 'b': 200, 'c': 300}
+d2 = {'a': 300, 'b': 200, 'd': 400}
+print solution(d1, d2)
