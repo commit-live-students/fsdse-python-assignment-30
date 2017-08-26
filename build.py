69276380aa7d8e717aa2f2e49faa292c95e73e7f
backtrackbaba <sai.cosmo@gmail.com>
2017-08-26 10:34:46 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,10 @@
 def solution(dic1, dic2):
-    """Enter Code Here"""
+    for i in dic2:
+        if i in dic1:
+            dic1[i] = dic1[i] + dic2[i]
+            #print d1[i]
+    return dict(d2, **d1)
+
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+solution(d1,d2)
